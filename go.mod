91fe809ce41a170e18965a43ac6228ff24450081
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2024-02-04 16:17:01 -0800

    more stuff

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/intangiblematter/gdman
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to GDMan! The Godot Version Manager")
+}
